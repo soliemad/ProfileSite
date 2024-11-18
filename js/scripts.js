d336427d0599bb8a317a857d3267a7e068d886dc
Emad Soliman <soliman.emad@hotmail.com>
2024-11-17 21:23:25 -0500

    more tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,43 @@ function toggleTheme() {
         document.getElementById("theme-icon").src = "media/icons/light_mode.svg";
     }
 }
+
+function scrollCarousell(direction) {
+    const scrollAmount = 900; // Adjust the scroll distance as needed
+    const list = document.querySelector('#experience .carousell .job-list');
+    const scrollLeft = list.scrollLeft;
+
+    if (direction === 'left') {
+        list.scrollTo({
+            left: scrollLeft - scrollAmount,
+            behavior: 'smooth'
+        });
+
+    } else if (direction === 'right') {
+        list.scrollTo({
+            left: scrollLeft + scrollAmount,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function updateIndicators() {
+    const indicators = document.querySelectorAll('.indicator');
+    const list = document.querySelector('#experience .carousell .job-list');
+    const scrollLeft = list.scrollLeft;
+    const scrollWidth = list.scrollWidth;
+    const clientWidth = list.clientWidth;
+    const scrollPercentage = (scrollLeft / (scrollWidth - clientWidth)) * 100;
+
+    indicators.forEach((indicator, index) => {
+        if (scrollPercentage >= index * 25 && scrollPercentage < (index + 1) * 25) {
+            indicator.classList.add('active');
+        } else {
+            indicator.classList.remove('active');
+        }
+    });
+}
+
+function toggleNav() {
+    document.querySelector('.navigation').classList.toggle('hidden');
+}
